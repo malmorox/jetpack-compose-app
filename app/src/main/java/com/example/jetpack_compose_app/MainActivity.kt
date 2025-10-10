64e8055039d4f1338bd9cdf80ba2030e1cbb50e8
malmorox <marcos.almorox@gmail.com>
2025-10-10 09:18:53 +0200

    probando

--- app/src/main/java/com/example/jetpack_compose_app/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_app/MainActivity.kt
@@ -6,12 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetpack_compose_app.ui.theme.JetpackcomposeappTheme
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +25,54 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetpackcomposeappTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                appTheme()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun button(name: String, modifier: Modifier = Modifier) {
+    Button(onClick = { }, modifier = modifier) {
+        Text(text = "Hello $name!")
+    }
+}
+
+@Composable
+fun listItem(messages: List<String>) {
+    LazyColumn {
+        items(messages) { message ->
+            Row() {
+                Text(text = message)
+            }
+            HorizontalDivider()
+        }
+    }
+}
+
+@Composable
+fun appTheme() {
+    Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            button(
+                name = "Android",
+                modifier = Modifier.padding(innerPadding)
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            var messages = listOf("Hello", "Hi", "Greetings", "Salutations", "Hola")
+            listItem(messages)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetpackcomposeappTheme {
-        Greeting("Android")
+        appTheme()
     }
 }
